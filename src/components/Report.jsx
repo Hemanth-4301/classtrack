e0c9d5757916dc08010aedb45b7af07791195584
Hemanth-4301 <hemanthachar526@gmail.com>
2024-12-26 19:52:52 +0530

    url changes

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
-import { pdfjs } from "react-pdf";
 import { motion } from "framer-motion";
 
 const Report = ({ file }) => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
   const [pdfFile, setPdfFile] = useState(file);
-
-  // Handle when the document is loaded
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
 
   return (
     <div>
@@ -24,7 +16,7 @@ const Report = ({ file }) => {
       >
         <b>Report</b>
       </motion.h1>
-      <div className="max-w-4xl mx-auto  p-4 ">
+      <div className="max-w-4xl mx-auto p-4">
         <div
           style={{
             maxHeight: "600px",
@@ -32,16 +24,15 @@ const Report = ({ file }) => {
           }}
           className="border-2 border-black rounded-lg p-3"
         >
-          <Worker
-            workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}
-          >
+          {/* Use the matching worker version */}
+          <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
             <Viewer fileUrl={pdfFile} />
           </Worker>
         </div>
 
         <div className="my-10 text-center">
           <a href={pdfFile} download="report.pdf">
-            <button className="px-6 py-3 bg-black border-2 border-black  text-white rounded-lg hover:bg-transparent hover:text-black">
+            <button className="px-6 py-3 bg-black border-2 border-black text-white rounded-lg hover:bg-transparent hover:text-black">
               Download PDF
             </button>
           </a>
